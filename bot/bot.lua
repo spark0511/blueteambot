5149e0af898ea196c9bea1cf43596b4694d764a5
spark0511 <alas1380@gmail.com>
2015-12-01 01:52:25 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -236,8 +236,8 @@ function create_config( )
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id ()
-  our_id = 
+function on_our_id (75517656)
+  our_id =75517656
 end
 
 function on_user_update (user, what)
